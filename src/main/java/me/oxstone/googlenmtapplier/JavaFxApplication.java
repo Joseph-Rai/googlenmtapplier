b478a63284258e719485a3fc50ffef8681778870
Joseph Rai <zbflzlxl@gmail.com>
2023-03-30 14:51:57 +0900

    Add Translate Module Using API Server Because some user work in secure environment blocking socket communication.

--- src/main/java/me/oxstone/googlenmtapplier/JavaFxApplication.java
+++ src/main/java/me/oxstone/googlenmtapplier/JavaFxApplication.java
@@ -13,10 +13,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class JavaFxApplication extends Application {
 
     private ConfigurableApplicationContext applicationContext;
-    public static final String PROGRAM_VER = "Google NMT Applier Ver 2.3.1";
+    public static final String PROGRAM_VER = "Google NMT Applier Ver 2.4.0";
     public static final String PROGRAM_AUTHOR = "oxstone7@gmail.com";
     public static final String PROGRAM_COPYRIGHT = "World Mission Society Church of God";
-    public static final String PROGRAM_LAST_MODIFIED = "04 Jan 2023";
+    public static final String PROGRAM_LAST_MODIFIED = "30 Mar 2023";
 
     @Override
     public void init() {

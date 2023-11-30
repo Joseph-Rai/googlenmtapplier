ba3791e79bace12925d972857422ae154b197e3e
oxstone7 <oxstone7@gmail.com>
2023-11-30 17:49:42 +0900

    bug fix

--- src/main/java/me/oxstone/googlenmtapplier/nmtmodule/NmtModule.java
+++ src/main/java/me/oxstone/googlenmtapplier/nmtmodule/NmtModule.java
@@ -3,7 +3,6 @@ package me.oxstone.googlenmtapplier.nmtmodule;
 import com.google.protobuf.InvalidProtocolBufferException;
 
 import javax.management.RuntimeErrorException;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
 
